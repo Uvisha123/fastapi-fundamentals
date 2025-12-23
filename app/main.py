5ff84ded8a409e1e610b881eb3571fde3b1b5a03
Uvisha Fernando <uvishafernando@gmail.com>
2025-12-23 12:08:49 +0530

    Implement GET endpoint for retrieving items by ID

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-from app.routes import health
+from app.routes import health,items
 
 app = FastAPI(title="FastAPI Fundamentals")
 
 app.include_router(health.router)
+
+app.include_router(items.router)
